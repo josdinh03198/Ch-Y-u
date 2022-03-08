47465e829d24d45b042809848362313418bb3e0a
josdinh03198 <72588136+josdinh03198@users.noreply.github.com>
2022-03-08 18:54:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Ngày Quốc Tế Phụ Nữ';
+const titleCard = 'Chị Yêu';
+const contentCard = 'Chúc Chị một ngày mùng 8/3 thật ý nghĩa, vui tươi ngập tràn hạnh phúc. Chúc Chị luôn cười tươi, cười duyên, cười e thẹn, cười trẻ trung, ngập tràn các kiểu cười trong ngày hôm nay.';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
